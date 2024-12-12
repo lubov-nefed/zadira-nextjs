46c640f7a52856213ef02a58b9c85bec150a5e94
lubov-nefed <love.nefed@gmail.com>
2024-12-12 17:47:17 +0300

    feat: add LogOut styling

--- components/LogOut.tsx
+++ components/LogOut.tsx
@@ -4,6 +4,7 @@ import { deleteSessionCookies } from "@/auth/actions";
 export default function LogOut() {
   return (
     <button
+      className="border border-gray-500 rounded px-2 pb-1 hover:bg-blue-400 hover:text-white active:outline active:outline-2 active:outline-blue-900"
       onClick={async () => {
         console.log("LogOut");
         deleteSessionCookies();

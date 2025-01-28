9083c5a9a175dc76b8085eaaf7a12aed43ad5ddc
lubov-nefed <love.nefed@gmail.com>
2025-01-28 16:46:17 +0300

    refactor: add loging error to error component

--- app/error.tsx
+++ app/error.tsx
@@ -1,7 +1,16 @@
 "use client";
 import Link from "next/link";
+import { useEffect } from "react";
 
-export default function Error() {
+export default function Error({
+  error,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
   return (
     <h2>
       Something went wrong. <Link href="/"> Go to homepage</Link>
